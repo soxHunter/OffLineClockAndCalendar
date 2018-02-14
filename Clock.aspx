a18a70bf988d1b0d55263607f867a8edaa478a86
Nyammi <nayoshiok@yahoo.co.jp>
2018-02-14 16:49:34 +0900

    Test--First virgion

---
+++ Clock.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Clock.aspx.cs" Inherits="JsTest.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+    <link href="Content/Site.css" rel="stylesheet" />
+    <link href="Content/themes/base/css/fontawesome.css" rel="stylesheet" />
+    <link href="Content/themes/base/css/fa-brands.css" rel="stylesheet" />
+    <link href="Content/themes/base/css/fa-regular.min.css" rel="stylesheet" />
+    <link href="Content/themes/base/css/fa-solid.css" rel="stylesheet" />
+    <link href="Content/themes/base/css/fa-solid.min.css" rel="stylesheet" />
+    <link href="Content/themes/base/css/fa-svg-with-js.css" rel="stylesheet" />
+    <link href="Content/themes/base/css/fontawesome.min.css" rel="stylesheet" />
+    <link href="Content/themes/base/css/fontawesome-all.css" rel="stylesheet" />
+    <link href="Content/themes/base/css/fontawesome.min.css" rel="stylesheet" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Clock</title>
+</head>
+<body style="margin:0px;padding:0;">
+    <script src="Scripts/jquery-1.7.1.min.js"></script>
+    <p id="TodayDateLabel" style="font-size:40px; margin:0;padding:0;text-align:center;float:left;">
+        <script>
+            var TodayDate = new Date();
+            var y = TodayDate.getFullYear();
+            var m = TodayDate.getMonth()+1;
+            var w = TodayDate.getDay();
+            var d = TodayDate.getDate();
+
+            var wk = new Array("日", "月", "火", "水", "木", "金", "土");
+            document.write(y + " 年  " + m + " 月  " + d + " 日  （" + wk[w] +"）");
+        </script>
+    </p>
+    <div>
+        <div style="text-align:left;margin:0;padding:0;position:fixed;left:-80px;top:100px;"> 
+            <canvas id="canvas" style="margin:0;padding:0;width:1000px;"></canvas>
+        
+            <script src="Scripts/ClockCanvas.js"></script>
+        </div>
+    
+        <div style="position:fixed;left:800px;top:40px;">
+
+                <asp:Literal ID="ThisMonthCurrendar" runat="server"></asp:Literal>
+
+        </div>
+    </div>
+        <footer style="position:fixed;bottom:0px;"><i class="fa fa-copyright"></i> 2018 YOSHIOKA, Nami</footer>
+</body>
+</html>
---
+++ Clock.aspx.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JsTest
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.ThisMonthCurrendar.Text = this.CalendarHtml(0)+ this.CalendarHtml(1);
+        }
+        /// <summary>
+        /// カレンダ要素の日付配列を取得する
+        /// </summary>
+        /// <param name="AddMonth"></param>
+        /// <returns></returns>
+        public string[] CalendarElements(int AddMonth)
+        {
+            // 月の最初日
+            DateTime FirstDay = new DateTime(DateTime.Today.Year,DateTime.Today.Month+AddMonth,1);
+            // 翌月最初日
+            DateTime NextMonthFirstDay = new DateTime(DateTime.Today.Year, DateTime.Today.Month + AddMonth + 1, 1) ;
+            // 月の最終日
+            DateTime LastDay = NextMonthFirstDay.AddDays(-1);
+            
+            int intLastDay = LastDay.Day;
+            
+            // 返却値
+            string[] Arr = new string [0] ;
+            
+            // 日付
+            int Count= 1;
+
+            for (int i=0; i<=42; i++){
+                if (i < (int)FirstDay.DayOfWeek)
+                {
+                    Array.Resize(ref Arr, i + 1);
+                }
+                else if (Count <= intLastDay)
+                    
+                {
+                    Array.Resize(ref Arr, i + 1);
+                    Arr[i] = Count.ToString();
+                    Count = Count+1;
+                 }
+            }
+            return Arr;            
+        }
+        /// <summary>
+        /// HTMLを書く
+        /// </summary>
+        /// <param name="AddMonth"></param>
+        /// <returns></returns>
+        public string CalendarHtml(int AddMonth)
+        {
+
+            // 月
+            DateTime DateMonth = new DateTime(DateTime.Today.Year, DateTime.Today.Month + AddMonth , 1);
+            int month = DateMonth.Month;
+
+            // 年
+            int year = DateMonth.Year;
+
+            string HTML = "<h1><small>" + year + "<small>年</small>&nbsp;&nbsp;</small>" 
+                + month + "<small>月</small></h1>";
+            HTML+="<Table class=\"CalendarTable\">";
+            HTML += "<TH class=\"RedFont\">日</TH>";
+            HTML += "<TH>月</TH>";
+            HTML += "<TH>火</TH>";
+            HTML += "<TH>水</TH>";
+            HTML += "<TH>木</TH>";
+            HTML += "<TH>金</TH>";
+            HTML += "<TH class=\"BlueFont\">土</TH>";
+            string[] Arr = CalendarElements(AddMonth);
+
+            for (int i = 0; i<Arr.Length;i++)
+            {
+                // 週はじめ日（日曜日）→行を追加
+                if (i % 7 == 0) {
+                    if (i != 0)
+                    {
+                        HTML += "</TR><TR>";
+                    }
+                    else
+                    {
+                        HTML += "<TR>";
+                    }
+                }
+                // セル開くタグ入力
+                string HtmlPart = "<TD";
+                // ブランクセル
+                if (Arr[i] == null)
+                {
+                    HtmlPart += "><BR />";
+                } 
+                // 今日の日付の書式設定
+                else if (year == DateTime.Today.Year 
+                    && month == DateTime.Today.Month && DateTime.Today.Day==Int32.Parse(Arr[i]))
+                {
+                    HtmlPart += " style=\"background-color:yellow\"";
+                }
+                // 日曜日の書式・値設定
+                if (i % 7 == 0 && Arr[i] != null)
+                {
+                    HtmlPart += " class=\"RedFont\">" + Arr[i];
+                }
+                // 土曜日の書式・値設定
+                else if (i % 7 == 6 && Arr[i] != null)
+                {
+                    HtmlPart += " class=\"BlueFont\">" + Arr[i];
+                }
+                // 書式設定なし・値設定
+                else if (Arr[i] != null)
+                {
+                    HtmlPart += ">" + Arr[i];
+                }
+                // セル閉じるタグ
+                HTML += HtmlPart + "</TD>";
+            }
+            // テーブル閉じるタグ
+            HTML += "</TR></TABLE>";
+                      
+            return HTML;
+        }
+    }
+}
